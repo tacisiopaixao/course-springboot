a88212b3749bb5c38bbd839229008eb16aabc26d
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2021-10-15 10:03:04 -0300

    Project created

---
+++ src/main/java/com/api/gel/GelApplication.java
@@ -0,0 +1,13 @@
+package com.api.gel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GelApplication.class, args);
+	}
+
+}
