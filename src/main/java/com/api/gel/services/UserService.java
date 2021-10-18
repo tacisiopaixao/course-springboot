506ad73d2afbbb56c17107151cb4c89aa01ad791
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2021-10-18 11:29:30 -0300

    Camada de serviço, registro de componentes

---
+++ src/main/java/com/api/gel/services/UserService.java
@@ -0,0 +1,27 @@
+package com.api.gel.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.gel.entities.User;
+import com.api.gel.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+		public User findById(long id){
+		Optional <User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
