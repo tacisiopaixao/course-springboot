ee1526e71fa4a89982de086f3ceda1dbaad62500
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2021-10-28 12:03:08 -0300

    OrderStatus enum

--- src/main/java/com/api/gel/config/TestConfig.java
+++ src/main/java/com/api/gel/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.api.gel.entities.Order;
 import com.api.gel.entities.User;
+import com.api.gel.entities.enums.OrderStatus;
 import com.api.gel.repositories.OrderRepository;
 import com.api.gel.repositories.UserRepository;
 
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner  {
 		User u1 = new User(null, "Joao", "jaoa@gmail.com", "99775566", null, null);
 		User u2 = new User(null, "Pedro", "pedro@gmail.com", "99775566", null, null);
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1); 
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		
--- src/main/java/com/api/gel/entities/Order.java
+++ src/main/java/com/api/gel/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.api.gel.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 
@@ -25,6 +26,8 @@ private static final long serialVersionUID = 1L;
 	private Long id;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name= "client_id")
@@ -34,10 +37,11 @@ private static final long serialVersionUID = 1L;
 		
 }
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -56,6 +60,17 @@ private static final long serialVersionUID = 1L;
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+		}
+	}
+		
 
 	public User getClient() {
 		return client;
