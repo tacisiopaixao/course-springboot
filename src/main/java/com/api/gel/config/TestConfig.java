ce8af6cef5319f6e0758f7aa7a623411c0ebb54a
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2021-11-07 16:32:54 -0300

    Product-OrderItem one-to-many association -coreção de erros

--- src/main/java/com/api/gel/config/TestConfig.java
+++ src/main/java/com/api/gel/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.api.gel.entities.Category;
 import com.api.gel.entities.Order;
 import com.api.gel.entities.OrderItem;
+import com.api.gel.entities.Payment;
 import com.api.gel.entities.Product;
 import com.api.gel.entities.User;
 import com.api.gel.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner  {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+	
+		orderRepository.save(o1);
 	}
 
 }
--- src/main/java/com/api/gel/entities/OrderItem.java
+++ src/main/java/com/api/gel/entities/OrderItem.java
@@ -32,6 +32,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
