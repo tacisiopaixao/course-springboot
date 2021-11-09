53ab64811b083a1f427ff31a5b8836571235133b
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2021-11-08 23:40:51 -0300

    Exception handling - delete

--- src/main/java/com/api/gel/services/UserService.java
+++ src/main/java/com/api/gel/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.api.gel.entities.User;
 import com.api.gel.repositories.UserRepository;
+import com.api.gel.services.excepitions.DatabaseException;
 import com.api.gel.services.excepitions.ResourceNotFoundException;
 
 @Service
@@ -30,9 +33,15 @@ public class UserService {
 	
 	}
 	public void delete(Long id) {
-		repository.deleteById(id);
-	}
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 
+	}
 	public User update(Long id, User obj){
 		User entity = repository.getById(id); 
 		updateData(entity, obj);
