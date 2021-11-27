e4e039bf24d780005acbe834bbca74c010025de5
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2021-11-27 10:11:06 -0300

    Exception handling - update

--- src/main/java/com/api/gel/services/UserService.java
+++ src/main/java/com/api/gel/services/UserService.java
@@ -3,6 +3,8 @@ package com.api.gel.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -25,13 +27,14 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
 		return repository.save(obj);
-	
+
 	}
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
@@ -42,10 +45,16 @@ public class UserService {
 		}
 
 	}
-	public User update(Long id, User obj){
-		User entity = repository.getById(id); 
-		updateData(entity, obj);
-		return repository.save(entity);
+
+	public User update(Long id, User obj) {
+
+		try {
+			User entity = repository.getById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
